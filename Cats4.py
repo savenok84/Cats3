6a6171e4eef66ff6c2c37ba0298cff576b15eb66
unknown <savelyevanadezda18@gmail.com>
2025-07-31 02:12:00 +0300

    Initial commit - Cats3 project

---
+++ Cats4.py
@@ -0,0 +1,25 @@
+from tkinter import*
+from PIL import Image, ImageTk
+import requests
+from io import Bytes IO
+
+def load_image:
+    try responce = requests.get(url)
+
+window = Tk ()
+window.title ("Cats")
+window.geometry ("600x480")
+
+
+label=Label ()
+label.pack ()
+
+url = "https://cataas.com/cat"
+img = load_image (url)
+
+
+if img:
+    label.config (image=img)
+    label.image = img #если не прописать, сборщик мусора удалит картинку
+
+window.mainloop()
